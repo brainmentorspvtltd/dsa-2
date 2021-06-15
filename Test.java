1d03b746c43c02f33bc339d274a36a0aa078b82c
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-06-15 16:15:38 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Test {
+    public static void main(String[] args) {
+        
+        int size=5;
+        int arr[]=new int[size];
+        arr[0]=56;
+        arr[1]=90;
+        int arr2[]={10,20,30};
+        int []arr3={10,20,30};
+        int []arr4;
+        arr3=new int[size];
+
+       // int []arr4=new int[5]{4,5,6,7,8};
+    }
+}
